72ac84f8369773588ee10d6e4081ee351af56808
tselofan@yandex.ru <tselofan@yandex.ru>
2016-03-29 17:55:01 +0300

    Добаивл раскраску в отображении места

---
+++ Assets/ColorSelect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSelect : MonoBehaviour {
+
+    public Color color = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/GuiInfo.cs
+++ Assets/GuiInfo.cs
@@ -24,6 +24,13 @@ public class GuiInfo : MonoBehaviour {
     {
         public string name;
         public float progress;
+        public Color color;
+    }
+
+    static string colorToHex(Color32 color)
+    {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
     }
 
     void OnGUI()
@@ -32,20 +39,23 @@ public class GuiInfo : MonoBehaviour {
         var ordered = Cars.Where(c => c != null).Select(c=>new or()
         {
             name =  ((WaypointProgressTracker)c.GetComponent("WaypointProgressTracker")).Circuit.gameObject.name,
-            
+            color = ((ColorSelect)c.GetComponent("ColorSelect")).color,
             progress =((CriterianProgress)c.GetComponent("CriterianProgress")).progressDistance 
         })
         .OrderByDescending(c => c.progress);
         
-        string str = string.Join("\n",ordered.Select((c,index) =>string.Format("{0}. \t {1} - прогресс {2}", index.ToString(), c.name, c.progress.ToString())).ToArray());
+        string str = string.Join("\n",ordered.Select((c,index) =>string.Format("<color='#{3}'>{0}. \t {1} - прогресс {2,5:N1}</color>", index.ToString(), c.name, c.progress, colorToHex(c.color))).ToArray());
         var currentStyle = new GUIStyle(GUI.skin.box);
         currentStyle.alignment = TextAnchor.UpperLeft;
+        currentStyle.richText = true;
         GUI.Box(
             new Rect(0, 0, 300, 100), string.Format("Позиция: \n {0}", str)
             ,currentStyle
             //   ,new GUIStyle() { alignment = TextAnchor.UpperLeft, border = new RectOffset(1, 1, 1, 1)}
             );
-        Debug.Log(str);
+
+        
+        //Debug.Log(str);
 
 
     }
